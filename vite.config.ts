149d170d7ce4de4b601e21518d83e63b059fec73
Chondan Susuwan <chondansusuwan@outlook.com>
2025-10-05 20:02:01 +0700

    Setup storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import path, { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
     envDir: resolve(__dirname, './env'),
     envPrefix: 'VITE_',
@@ -20,4 +26,32 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    test: {
+        projects: [
+            {
+                extends: true,
+                plugins: [
+                    // The plugin will run tests for the stories defined in your Storybook config
+                    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+                    storybookTest({
+                        configDir: path.join(dirname, '.storybook'),
+                    }),
+                ],
+                test: {
+                    name: 'storybook',
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: 'playwright',
+                        instances: [
+                            {
+                                browser: 'chromium',
+                            },
+                        ],
+                    },
+                    setupFiles: ['.storybook/vitest.setup.ts'],
+                },
+            },
+        ],
+    },
 });
