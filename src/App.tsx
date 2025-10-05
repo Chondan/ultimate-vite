f058b8c8fee8935588cc558a9e9ce610485f0a0c
Chondan Susuwan <chondansusuwan@outlook.com>
2025-10-05 22:37:27 +0700

    Setup landing page using shadcn and tailark
    Ref:
    1. https://ui.shadcn.com/
    2. https://tailark.com/

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import '@/App.scss';
+import HeroSection from '@/components/hero-section';
+import { HeroHeader } from './components/header';
+import Features from './components/features-1';
+import CallToAction from './components/call-to-action';
+import FooterSection from './components/footer';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
-    return <div className='font-bold text-red-500 m-20'>Hello World</div>;
+    return (
+        <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+            <main>
+                <HeroHeader />
+                <HeroSection />
+                <Features />
+                <CallToAction />
+                <FooterSection />
+            </main>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/components/README.md
@@ -0,0 +1,2 @@
+https://ui.shadcn.com/docs/installation/vite
+https://tailark.com/hero-section
