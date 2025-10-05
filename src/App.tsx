36e7bb22e8639a793e5f4bbd4198865bcf41939a
Chondan Susuwan <chondansusuwan@outlook.com>
2025-10-05 19:46:32 +0700

    Setup env

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                     <img src={reactLogo} className='logo react' alt='React logo' />
                 </a>
             </div>
-            <h1 className={clsx('header', 'header-italic')}>Vite + React</h1>
+            <h1 className={clsx('header', 'header-italic')}>{import.meta.env.VITE_APP_NAME} + React</h1>
             <div className='card'>
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+    envDir: resolve(__dirname, './env'),
+    envPrefix: 'VITE_',
     resolve: {
         alias: {
             '@src': path.resolve(__dirname, './src'),
