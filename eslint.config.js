f2b32a6482886472b728d438c12938f9e177269e
Chondan Susuwan <chondansusuwan@outlook.com>
2025-10-05 19:16:41 +0700

    Setup eslint + prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,53 @@
-import js from "@eslint/js"
-import globals from "globals"
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import prettier from "eslint-plugin-prettier"
-import { defineConfig, globalIgnores } from "eslint/config"
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierRecommend from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      react.configs.flat.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    plugins: {
-      react,
-      prettier,
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
+    globalIgnores(["dist"]),
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            react.configs.flat.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+            prettierRecommend
+        ],
+        plugins: {
+            react,
+            prettier
         },
-      },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            indent: ["error", 4],
+            semi: ["error", "always"],
+            "jsx-quotes": ["error", "prefer-single"],
+            "max-len": ["error", { "code": 130, "tabWidth": 4 }],
+            "react/react-in-jsx-scope": "off",
+            "react/jsx-indent": ["error", 4, { checkAttributes: true }],
+            "react/jsx-indent-props": ["error", 4],
+            "prettier/prettier": "error",
+            "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+        }
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      indent: ["error", 4],
-      semi: ["error", "always"],
-      "jsx-quotes": ["error", "prefer-single"],
-      "max-len": ["error", { "code": 130, "tabWidth": 4 }],
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-indent": ["error", 4, { checkAttributes: true }],
-      "react/jsx-indent-props": ["error", 4],
-      "prettier/prettier": "error",
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }]
-    }
-  },
-])
+]);
